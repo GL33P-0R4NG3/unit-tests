b18fd9c069d38f1753f05767b09b1ec1c45a34e4
Kelmet <kelmetm@proton.me>
2025-02-20 11:40:55 +0100

    added some sources (without tests)

--- main.py
+++ main.py
@@ -8,41 +8,15 @@ logging.basicConfig(
     datefmt="%d-%m-%Y %H:%M:%S"
 )
 
-def main():
-    # TODO: we need to make more messages
-    MESSAGES = [
-        "I can read your mind",
-        "I can buy you flowers",
-        "I can shower with you",
-        "I can take you hiking",
-        "I can send you letter",
-        "I can use toilet",
-        "I can bake you cake"
-    ]
+def multiply(x, y):
+    return x + y
 
-    print("Working...")
+def divide(x, y):
+    return x / y
 
-    logging.info("Script started...")
-    logging.debug("I do many things >:D!")
-
-    i = 0
-
-    while True:
-        a = random.randint(0, 314)
-        if a % 6 == 0:
-            logging.debug("They are calling... See you later!")
-            break
-        if a % 2 == 0:
-            logging.debug(MESSAGES[random.randint(0, len(MESSAGES) - 1)])
-        if i == 10:
-            logging.warning("Wow we hit %d. Such a big number", i)
-        if a % 10 == 0:
-            logging.error("Exceeded precise calculation your results might not be accurate!")
-        i += 1
-
-    logging.info("Script Ended")
-
-    print("Log Saved!")
+def sqrt(x):
+    return x ** (1 / 2)
 
 if __name__ == "__main__":
-    main()
+    a = 4 - multiply(2,2)
+    print(a)
